84448376dc13ae4672e45a7cef021f16139cf900
kikuKeii <miftakhuddinfalaki@gmail.com>
2024-01-12 16:25:53 +0700

    Init

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(".button_hidden").click(function () {
+  $(".sidebar_doc").hide(500);
+  $(".show_sidebar").show(500);
+});
+$(".button_show").click(function () {
+  $(".sidebar_doc").show(500);
+  $(".show_sidebar").hide(500);
+});
